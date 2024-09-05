4720536ee8b09ed547bf4973c8984de7ec23bda6
Angel Luis <alsaorin9987@gmail.com>
2024-09-05 19:20:55 +0200

    Enunciados Ejercicios Estructuras Condicionales

---
+++ 3.Ejs_Estructuras_Condicionales/Ejercicio_1.py
@@ -0,0 +1,8 @@
+"""
+        >>>>>>>    Ejercicios de Estructuras Condicionales   <<<<<<
+                                                             """
+                                                             
+print (f">>>>>>>    Ejercicios de Estructuras Condicionales   <<<<<<\n")                                                           
+print ("""Ejercicio 1: Escribir un programa que almacene la cadena de caracteres password en una variable, 
+pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el usuario 
+coincide con la guardada en la variable.\n""")
