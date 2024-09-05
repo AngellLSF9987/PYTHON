ed50197c28c2a3568a11bd999b0a2843706ebd29
Angel Luis <alsaorin9987@gmail.com>
2024-09-05 18:58:33 +0200

    Ejercicio_3 Extras Completado

--- Ejs_Extras/Ejercicio_3.py
+++ Ejs_Extras/Ejercicio_3.py
@@ -11,3 +11,27 @@ redondeado con dos decimales.
                         Imc = peso/estatura**2
                 Redondear con 2 decimales => round(variable,2) """)
 
+print (f"     -> Variable para el peso > p <")
+print (f"     -> Variable para la estatura > e <\n\n")
+
+print (f"El programa pedirá al usuario que introduzca su peso y estatura")
+print (f"Por tanto, deberán crearse 2 input, 1 para cada valor.\n")
+
+
+p = input("¿Introduzca su peso?\n")
+e = input("\n¿Introduzca su estatura?\n")
+
+print (f"El programa mostrará los datos introducidos por el usuario")
+
+print (f"Sus datos introducidos son:")
+print ("        - Su peso: ",p)
+print ("        - Su estatura: ",e)
+
+print (f"\nPor último, el programa calculará el índice de masa corporal del usuario y lo redondeará a 2 decimales")
+
+imc = (float(p)/float(e))**2
+
+print (f"Su índice de masa corporal es: ", round(imc,2),"\n")
+
+
+
