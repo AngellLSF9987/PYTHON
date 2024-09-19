1074a5cf2b5721ac534370a02523288483afa46f
Angel Luis <alsaorin9987@gmail.com>
2024-09-19 17:43:34 +0200

    Ejercicio Extra

---
+++ UD.2/5.Ejs_Tuplas/5.2.Ej_Tuplas_y_Listas_Extra/Ejercicio_Extra.py
@@ -0,0 +1,34 @@
+"""
+        >>>>>>>    Ejercicio Extra de Tuplas y Listas   <<<<<<
+                                                             """
+print (f">>>>>>>    Ejercicios de Tuplas   <<<<<<")                                                            
+print (f"\n","Ejercicio Extra: \n")
+print ("""
+                        LORETIX
+
+Crear un programa para gestionar la plataforma de video LORETIX.
+
+    A. Crear una lista de usuarios con sus contraseñas para poder acceder a la
+       plataforma, y una para el administrador. (Lista con tuplas anidadas)
+
+        a. El usuario tendrá acceso a unas opciones, mientras que el
+           administrador a otras.
+
+    B. Crear una lista de películas: Define una lista llamada cartelera y agrega
+       al menos 5 películas diferentes. Por ejemplo: Nombre, genero, duración.
+
+    C. Menú con las diferentes opciones.
+
+        Usuario:
+
+            A. Buscar película por título.
+            B. Buscar películas por género.
+            C. Cambiar contraseña de usuario
+            D. Crear listas de películas favoritas que se quiere ver.
+            E. Mostrar la lista de películas favoritas.
+
+        Administrador:
+
+            A. Agregar una nueva película.
+            B. Modificar el contenido de la lista de películas.
+            C. Eliminar una película de la lista.    \n""")
